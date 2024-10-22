ed408cebf428a23fa499f4d3402cd36057ab839e
Nigam Katta <nigamkatta@gmail.com>
2024-10-21 23:49:23 -0400

    build changes

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/home/alien/D/D1/deepika-asandi.github.io/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
   "component---src-templates-post-js": hot(preferDefault(require("/home/alien/D/D1/deepika-asandi.github.io/src/templates/post.js"))),
   "component---src-templates-tag-js": hot(preferDefault(require("/home/alien/D/D1/deepika-asandi.github.io/src/templates/tag.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/alien/D/D1/deepika-asandi.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/alien/D/D1/deepika-asandi.github.io/src/pages/404.js"))),
   "component---src-pages-archive-js": hot(preferDefault(require("/home/alien/D/D1/deepika-asandi.github.io/src/pages/archive.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/alien/D/D1/deepika-asandi.github.io/src/pages/index.js"))),
